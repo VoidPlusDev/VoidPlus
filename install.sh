2bf6628c4e6d68694f307dea77b675b5c4dc52e0
VoidPlusDev <x3710315@gmail.com>
2026-06-21 12:25:12 +0530

    install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+#mkdir -p $PREFIX/etc/apt/sources.list.d
+
+echo "deb [trusted=yes] https://raw.githubusercontent.com/VoidPlusDev/VoidPlus/refs/heads/main  voidplus main" > $PREFIX/etc/apt/sources.list.d/voidplus.list
+yes | apt install wget 
+wget https://github.com/VoidPlusDev/VoidPlus/blob/main/VoidPlus.key
+
+apt-key add VoidPlus.key 
+
+mv /data/data/com.termux/files/usr/etc/apt/trusted.gpg /data/data/com.termux/files/usr/etc/apt/trusted.gpg.d
+
+apt update
+
+echo "VoidPlus repository installed!"
